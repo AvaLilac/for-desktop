ccf04bfce9bed1f3979ecf90687931840a3ec610
AvaLilac <amyshimplays@gmail.com>
2026-02-26 06:46:19 -0500

    Update forge.config.ts
    Signed-off-by: AvaLilac <amyshimplays@gmail.com>

--- forge.config.ts
+++ forge.config.ts
@@ -120,6 +120,11 @@ const config: ForgeConfig = {
           config: "vite.main.config.ts",
           target: "main",
         },
+        {
+          entry: "src/aviafavsystem.js",
+          config: "vite.main.config.ts",
+          target: "main",
+        },
       ],
       renderer: [],
     }),
