4f13d893cd976596246c1dcd97197b1192248453
Amelia Frost <afrosty.skye@gmail.com>
2026-04-06 16:33:57 -0700

    New Icon

--- forge.config.ts
+++ forge.config.ts
@@ -98,6 +98,11 @@ if (!process.env.PLATFORM) {
 
 const customVitePluginBuild: VitePluginBuildConfig[] = [
   {
+    entry: "avia_assets/icon.png",
+    config: "vite.main.config.ts",
+    target: "main",
+  },
+  {
     entry: "about.html",
     config: "vite.main.config.ts",
     target: "main",
