86a255246a69b3b6bdbdd30312d2bf36a3e8b3b0
Amelia Frost <afrosty.skye@gmail.com>
2026-07-28 13:44:29 -0700

    Fix AviaClient version in settings

--- avia_core/aviadesktopversion.js
+++ avia_core/aviadesktopversion.js
@@ -3,35 +3,31 @@
   window.__AVIA_DESKTOP_PATCH__ = true;
 
   function patchButton() {
-    document
-      .querySelectorAll("a.pos_relative.gap_16px.p_13px")
-      .forEach((el) => {
-        if (el.dataset.aviaPatched) return;
+    const el = document.querySelector(".settings_sidebar .content > div");
 
-        const textContainer = el.querySelector(
-          "div.d_flex.flex-g_1.flex-d_column",
-        );
-        if (!textContainer) return;
+    if (!el || el.dataset.aviaPatched) return;
 
-        const nameDiv = textContainer.querySelector("div");
-        const versionSpan = textContainer.querySelector("span.lh_1rem");
+    const textContainer = el.lastElementChild;
+    if (!textContainer) return;
 
-        if (!nameDiv || !versionSpan) return;
-        if (!nameDiv.textContent.includes("Stoat for Desktop")) return;
+    const versionSpan = textContainer.querySelector("span:first-child");
+    const nameDiv = versionSpan?.nextElementSibling;
 
-        if (!versionSpan.textContent.includes("Version:")) return;
+    if (!nameDiv || !versionSpan) return;
+    if (!nameDiv.textContent.includes("Stoat for Desktop")) return;
 
-        const aviaVersion = window.native.versions.aviaClient();
-        const stoatVersion = window.native.versions.desktop();
+    if (!versionSpan.textContent.includes("Version:")) return;
 
-        el.dataset.aviaPatched = "true";
+    const aviaVersion = window.native.versions.aviaClient();
+    const stoatVersion = window.native.versions.desktop();
 
-        nameDiv.textContent = "Avia Client Desktop";
-        versionSpan.textContent = `Version ${aviaVersion} (Based on Stoat ${stoatVersion})`;
+    el.dataset.aviaPatched = "true";
 
-        textContainer.style.whiteSpace = "normal";
-        textContainer.style.overflow = "visible";
-      });
+    nameDiv.textContent = "Avia Client Desktop";
+    versionSpan.textContent = `Version ${aviaVersion} (Based on Stoat ${stoatVersion})`;
+
+    textContainer.style.whiteSpace = "normal";
+    textContainer.style.overflow = "visible";
   }
 
   const observer = new MutationObserver(patchButton);
