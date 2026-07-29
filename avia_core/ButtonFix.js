03dd6c44f5f6afda41f3d9f5b0d405958f4425fd
Amelia Frost <afrosty.skye@gmail.com>
2026-07-28 20:09:52 -0700

    Fix ButtonFix

--- avia_core/ButtonFix.js
+++ avia_core/ButtonFix.js
@@ -1,35 +1,41 @@
-(function () { 
-    if (window.__BUTTON_FIX__) return;
-    window.__BUTTON_FIX__ = true;
+(function () {
+  if (window.__BUTTON_FIX__) return;
+  window.__BUTTON_FIX__ = true;
 
-    function uninjectButton(button) {
-        if (button?.parentElement) {
-            button.parentElement.removeChild(button);
-        }
+  function getChatBarButtons(append) {
+    return document.querySelectorAll(
+      `.app_body main > div:last-child > div > div:last-child > div > div ${append ?? ""}`,
+    );
+  }
+
+  function uninjectButton(button) {
+    if (button?.parentElement) {
+      button.parentElement.removeChild(button);
     }
+  }
 
-    function hasGifButton() {
-        return [...document.querySelectorAll("button")].some(button =>
-            button.querySelector(".material-symbols-outlined")?.textContent.trim() === "gif"
-        );
-    }
+  function hasGifButton() {
+    return [
+      ...getChatBarButtons("button > span.material-symbols-outlined"),
+    ].some((button) => button?.textContent.trim() === "gif");
+  }
 
-    const observer = new MutationObserver(() => {
-        const injectedButtons = [];
+  const observer = new MutationObserver(() => {
+    const injectedButtons = [];
 
-        document.querySelectorAll("div").forEach(element => {
-            if (element.id?.includes("avia")&&element.parentElement!=document.body) {
-                injectedButtons.push(element);
-            }
-        });
-
-        if (!hasGifButton()) {
-            injectedButtons.forEach(uninjectButton);
-        }
+    getChatBarButtons().forEach((element) => {
+      if (element.id?.startsWith("avia-")) {
+        injectedButtons.push(element);
+      }
     });
 
-    observer.observe(document.documentElement, {
-        childList: true,
-        subtree: true
-    });
+    if (!hasGifButton()) {
+      injectedButtons.forEach(uninjectButton);
+    }
+  });
+
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
 })();
